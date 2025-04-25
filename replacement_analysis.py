5e252313cfd6cfb999d1830dfb21ed6afca7c3b7
FLawrence <faith.lawrence@nationalarchives.gov.uk>
2025-04-25 10:14:23 +0100

    Copying files with problems into separate folder for further analysis. Separate out error files and make count of files and errors.

--- replacement_analysis.py
+++ replacement_analysis.py
@@ -14,7 +14,7 @@
 #filename - if - at start then rerun
 
 from pathlib import Path
-import json, os, datetime, time, re
+import json, os, datetime, time, re, shutil
 import pandas as pd
 import matplotlib
 import xml.etree.ElementTree as et
@@ -27,26 +27,34 @@ def load_replacement_data (processing_root, folder):
     
     data_path = processing_root + "/processing/" + folder
     cache_path = processing_root + "/processing/cache"
+    date_error = 0
+    date_not_found = 0
+    parsing_error = 0
+    processing = 0
+    duplication = 0
+    no_match = 0
+    
 
     data_values = {}
     column_values = {"case": ["file", "pass", "last_modified", "citation_match", "corrected_citation/citation_match", "year", "URI", "is_neutral"], 
                      "leg":["file", "pass", "last_modified", "detected_ref", "href", "canonical_form"], 
                      "abb": ["file", "pass", "last_modified", "initials", "expanded"]
                     }
+    move_list = []
 
     if not any(Path(cache_path).glob("*.pkl")):
         try: 
             for file in Path(data_path).glob("*.txt"):     
-    
-    
+                processing += 1
                 
                 base_filename = Path(file).stem
                 
                 pass_num = 1
                 
                 if base_filename[0] == "-":
-                    base_filename = base_filename[1:]
+                    base_filename_trimmed = base_filename[1:]
                     pass_num = 2
+                    duplication += 1
                     
                 print("Processing file:" + str(base_filename))   
 
@@ -59,7 +67,7 @@ def load_replacement_data (processing_root, folder):
                         dict_keys = temp_dict.keys()                    
                         
                         for key in dict_keys:
-                            temp_list = [base_filename, pass_num, last_modified] + temp_dict[key]
+                            temp_list = [base_filename_trimmed, pass_num, last_modified] + temp_dict[key]
                             #print(temp_list)
                             
                             if key in data_values.keys() and key != "dates":
@@ -72,8 +80,11 @@ def load_replacement_data (processing_root, folder):
                 xml_file = Path(processing_root + "/xml-enriched-bucket", base_filename + ".xml") 
                 
                 if xml_file.is_file():               
-                    dates = get_date_values(processing_root + "/xml-enriched-bucket", base_filename)
-                    dates_temp_list = [base_filename, pass_num, last_modified, dates["hearing_date_1"], dates["hearing_date_2"], dates["judgment_date"]]   
+                    (dates, p_error, no_date, d_error) = get_date_values(processing_root + "/xml-enriched-bucket", base_filename, data_path)
+                    date_error += d_error
+                    date_not_found += no_date
+                    parsing_error += p_error
+                    dates_temp_list = [base_filename_trimmed, pass_num, last_modified, dates["hearing_date_1"], dates["hearing_date_2"], dates["judgment_date"]]   
                     
                     if "dates" in data_values.keys():      
                         dates_df = data_values["dates"]
@@ -84,19 +95,35 @@ def load_replacement_data (processing_root, folder):
                         dates_df = pd.DataFrame([dates_temp_list], columns=["file", "pass", "last_modified", "hearing_date_1", "hearing_date_2", "judgment_date"])
                         data_values["dates"] = dates_df
                 else:
-                    with open("data/errors.txt", "a") as myfile:
-                        myfile.write("Could not file " + base_filename + ".xml\n")                         
-
+                    with open("data/errors-NoMatchingFile.txt", "a") as myfile:
+                        myfile.write("Could not find " + base_filename + ".xml\n")  
+                    
+                    no_match += 1    
+                    move_list.append(base_filename + ".txt")                    
+            
+            for file_to_move in move_list:
+                try:
+                    #print("Moving " + str(Path(data_path, file_to_move)) + " to " + str(Path(data_path, "NoMatchingFile", file_to_move)))
+                    shutil.move(Path(data_path, file_to_move), Path(data_path, "NoMatchingFile", file_to_move))   
+                except Exception as e:
+                    print("Could not copy " + str(Path(data_path, file_to_move)) + " to " + str(Path(data_path, "NoMatchingFile", file_to_move)) + ": " + str(e))
                                 
             for key, dfs in data_values.items():
                 remove_dups = dfs.groupby(dfs.columns.tolist(), as_index=False).size()
                 #print(df.to_string())
                 #print(remove_dups.to_string())
-                remove_dups.to_pickle(Path(cache_path, key + ".pkl"))
+                
+                #remove_dups.to_pickle(Path(cache_path, key + ".pkl"))
                 data_values[key] = remove_dups
                     
         except OSError as e:
             print("Error in data loading: " + str(e))
+            
+            
+        print("Processed " + str(processing) + " files (" + str(duplication) + " repeats)")
+        print("XML files not parsable: " + str(parsing_error))
+        print("Dates not found: " + str(date_not_found))
+        print("Date extraction error: " + str(date_error))
     else:
         #print(Path(processing_folder, "cache"))
         
@@ -182,17 +209,29 @@ def get_date_references(dates):
     filtered_dates.to_csv("data/dates.csv")
     
 
-def get_date_values(folder, filename):
+def get_date_values(xml_folder, filename, processing_folder=""):
     #print(Path(folder, filename + ".xml"))
     
+    date_error = 0
+    date_not_found = 0
+    parsing_error = 0
     data = {}
     try:
-        tree = et.parse(Path(folder, filename + ".xml"))
+        tree = et.parse(Path(xml_folder, filename + ".xml"))
     except et.ParseError as e:
-        with open("data/errors.txt", "a") as myfile:
+        with open("data/errors-ParsingError.txt", "a") as myfile:
             myfile.write("Parser error in " + filename + ".xml: " + str(e) + "\n")
+        parsing_error += 1
+ 
+        if processing_folder != "":
+            try:
+                #print("Copying " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "ParsingError", filename + ".txt")))
+                shutil.copyfile(Path(processing_folder, filename + ".txt"), Path(processing_folder, "ParsingError", filename + ".txt")) 
+                shutil.copyfile(Path(xml_folder, filename + ".xml"), Path(xml_folder, "ParsingError", filename + ".xml")) 
+            except Exception as e:
+                print("Could not copy " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "ParsingError", filename + ".txt")) + ": " + str(e))
         
-        return {"judgment_date":"", "hearing_date_1":"", "hearing_date_2":""}
+        return ({"judgment_date":"", "hearing_date_1":"", "hearing_date_2":""}, parsing_error, date_not_found, date_error)
         
     root = tree.getroot()
     
@@ -203,10 +242,19 @@ def get_date_values(folder, filename):
     if m := re.match(r'{.*}', root.tag):
         ns = m.group(0)
     else:
-        with open("data/errors.txt", "a") as myfile:
+        with open("data/errors-ParsingError.txt", "a") as myfile:
             myfile.write("Could not find namespaces in " + filename + ".xml\n")
+        parsing_error += 1
+            
+        if processing_folder != "":
+            try:
+                #print("Copying " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "ParsingError", filename + ".txt")))
+                shutil.copyfile(Path(processing_folder, filename + ".txt"), Path(processing_folder, "ParsingError", filename + ".txt"))
+                shutil.copyfile(Path(xml_folder, filename + ".xml"), Path(xml_folder, "ParsingError", filename + ".xml")) 
+            except Exception as e:
+                print("Could not copy " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "ParsingError", filename + ".txt")) + ": " + str(e))
         
-        return {"judgment_date":"", "hearing_date_1":"", "hearing_date_2":""}        
+        return ({"judgment_date":"", "hearing_date_1":"", "hearing_date_2":""}, parsing_error, date_not_found, date_error)       
     
     judgment_date_string = ""
     
@@ -218,10 +266,20 @@ def get_date_values(folder, filename):
         try:
             data["judgment_date"] = datetime.datetime.strptime(judgment_date_string, '%Y-%m-%d')
         except ValueError as e:
-            with open("data/errors.txt", "a") as myfile:
+            with open("data/errors-DateError.txt", "a") as myfile:
                 myfile.write("Error in judgment date extraction in " + filename + ".xml: " + str(e) + "\n")
                 
             data["judgment_date"] = ""
+            date_error += 1
+            
+            if processing_folder != "":
+                try:
+                    #print("Copying " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "DateError", filename + ".txt")))
+                    shutil.copyfile(Path(processing_folder, filename + ".txt"), Path(processing_folder, "DateError", filename + ".txt"))
+                    shutil.copyfile(Path(xml_folder, filename + ".xml"), Path(xml_folder, "DateError", filename + ".xml")) 
+                except Exception as e:
+                    print("Could not copy " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "DateError", filename + ".txt")) + ": " + str(e))
+                
     else:
         data["judgment_date"] = ""
     
@@ -247,27 +305,48 @@ def get_date_values(folder, filename):
         except ValueError as e:
             match_results = str(m) + "\nGroup 1:\n" + str(m.group(1)) + "\nGroup 2\n" + str(m.group(2)) + "\nGroup 3:\n" + str(m.group(3)) + "\nGroup 4:\n" + str(m.group(4)) + "\nGroup 5:\n" + str(m.group(5)) + "\nGroup 6:\n" + str(m.group(6))  
             
-            with open("data/errors.txt", "a") as myfile:
+            with open("data/errors-DateError.txt", "a") as myfile:
                 myfile.write("Error in hearing date extraction in " + filename + ".xml. '" + match_results + "': " + str(e) + "\n")
-                
             data["hearing_date_1"] = ""
             data["hearing_date_2"] = ""
+            date_error += 1
+               
+            if processing_folder != "":
+                try:
+                    #print("Copying " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "DateError", filename + ".txt")))
+                    shutil.copyfile(Path(processing_folder, filename + ".txt"), Path(processing_folder, "DateError", filename + ".txt"))
+                    shutil.copyfile(Path(xml_folder, filename + ".xml"), Path(xml_folder, "DateError", filename + ".xml")) 
+                except Exception as e:
+                    print("Could not copy " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "DateError", filename + ".txt")) + ": " + str(e))                               
             
     else:
-        with open("data/errors.txt", "a") as myfile:
+        with open("data/errors-DateNotFound.txt", "a") as myfile:
             myfile.write("No hearing date match found in " + filename + ".xml\n")
         data["hearing_date_1"] = ""
-        data["hearing_date_2"] = ""
+        data["hearing_date_2"] = ""    
+        date_not_found += 1
+        
+        if processing_folder != "":
+            try:
+                #print("Copying " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "DateNotFound", filename + ".txt")))
+                shutil.copyfile(Path(processing_folder, filename + ".txt"), Path(processing_folder, "DateNotFound", filename + ".txt"))
+                shutil.copyfile(Path(xml_folder, filename + ".xml"), Path(xml_folder, "DateNotFound", filename + ".xml")) 
+            except Exception as e:    
+                print("Could not copy " + str(Path(processing_folder, filename + ".txt")) + " to " + str(Path(processing_folder, "DateNotFound", filename + ".txt")) + ": " + str(e))
 
     #print(data)
-    return data
+    return (data, parsing_error, date_not_found, date_error)
     
 
 
-open("data/errors.txt", 'w').close()
+open("data/errors-DateNotFound.txt", 'w').close()
+open("data/errors-DateError.txt", 'w').close()
+open("data/errors-ParsingError.txt", 'w').close()
+open("data/errors-NoMatchingFile.txt", 'w').close()
+
 processing_root = "data"
-data_folder = "replacements-bucket"
-#data_folder = "test-data"
+#data_folder = "replacements-bucket"
+data_folder = "test-data"
 
 datasets = load_replacement_data(processing_root, data_folder)
 
@@ -276,12 +355,12 @@ datasets = load_replacement_data(processing_root, data_folder)
 #    plot_dates(dataset)
 
 
-if 'leg' in datasets.keys():
-    get_legislation_references(datasets['leg'])
+#if 'leg' in datasets.keys():
+#    get_legislation_references(datasets['leg'])
 
 
-if 'case' in datasets.keys():    
-    get_cases_references(datasets['case'])
+#if 'case' in datasets.keys():    
+#    get_cases_references(datasets['case'])
 
 if 'dates' in datasets.keys(): 
     get_date_references(datasets['dates'])
